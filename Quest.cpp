6a9e4ff5ab4fd89f64bf75d9f245121e76224ecf
legionth <werwofl@googlemail.com>
2013-05-30 21:15:19 +0200

    Quest
    More attributes and methods

--- Quest.cpp
+++ Quest.cpp
@@ -1,12 +1,15 @@
 #include "Quest.h"
 
 
-Quest::Quest(sf::String questName,Item* item,std::map<int,int> need, std::map<int,int> exp)
+Quest::Quest(int id,sf::String questName,Item* item,std::map<int,int> need, std::map<int,int> exp, int money)
 {
 	this->need = need;
 	this->exp = exp;
 	this->questName = questName;
 	this->item = item;
+	this->id = id;
+	this->completed = false;
+	this->money = money;
 }
 
 
@@ -15,7 +18,9 @@ Quest::~Quest(void)
 }
 
 void Quest::action(Game* game){
+	if(isAvaible(game) && isVisible()){
 
+	}
 }
 
 void Quest::hide(){
@@ -33,11 +38,38 @@ void Quest::setVisible(bool visible){
 bool Quest::isAvaible(Game* game){
 	Player* player = game->getPlayer();
 	bool ret = false;
-
-
+	if(	this->need[MATH_ID] == player->getMath()				&&
+		this->need[ALGORITHM_ID] == player->getAlgortihm()		&&
+		this->need[PROGRAMMING_ID] == player->getProgramming()	&&
+		this->need[DESIGN_ID] == player->getDesign()			&&
+		this->need[DATABASES_ID] == player->getDatabases()		&&
+		this->need[ELECTRONICS_ID] == player->getElectronics()){
+			ret = true;
+	}
 	return ret;
 }
 	 
 Item* Quest::getItem(){
 	return this->item;
+}
+
+bool Quest::isVisible(){
+	return this->visible;
+}
+
+void Quest::setDescription(sf::String description){
+	this->description = description;
+}
+
+sf::String Quest::getQuestDescription(){
+	return this->description;
+}
+
+bool Quest::isComplete(){
+	return this->completed;
+}
+
+
+int Quest::getMoney(){
+	return this->money;
 }
--- Quest.h
+++ Quest.h
@@ -3,10 +3,15 @@
 #include "Game.h"
 #include "Item.h"
 
+/**
+* Quest Button returns Items and +/- Attrbiutes. Should be used to improve player skills and/or add a
+* Item to Inventory
+* Shoul work for all Menu's	@TODO test this :P
+*/
 class Quest
 {
 public:
-	Quest(sf::String questName,Item* item,std::map<int,int> need, std::map<int,int> exp);
+	Quest(int id,sf::String questName,Item* item,std::map<int,int> need, std::map<int,int> exp,int money=0);
 	~Quest(void);
 
 	void action(Game* game);
@@ -15,14 +20,28 @@ public:
 	void setVisible(bool visible);
 
 	bool isAvaible(Game* game);
+	bool isVisible();
+	bool isComplete();
 
+	void complete();
 	Item* getItem();
+
+	void setDescription(sf::String description);
+	sf::String getQuestDescription();
+
+	int getId();
+
+	int getMoney();
 private:
-	bool visible;
+	int id;
+	bool visible;										// If this quest is shown in Menu
 	Item* item;
 	sf::String questName;
+	sf::String description;
 
-	std::map<int,int> need;
-	std::map<int,int> exp;
+	std::map<int,int> need;								// Need Attributes by Player
+	std::map<int,int> exp;								// Experience reward for completed this quest
+	bool completed;										// Completed Quest
+	int money;
 };
 
